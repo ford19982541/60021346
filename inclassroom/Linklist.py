08b08a0b963516b4431477d6ee1dcc782fe53d25
ford19982541 <ford19982541@gmail.com>
2018-04-20 14:32:28 +0700

    add method additem print item

--- inclassroom/Linklist.py
+++ inclassroom/Linklist.py
@@ -1,9 +1,9 @@
 class Node :
-    dara =None 
+    data =None 
     nextNode = None
     
     def __init__(self,data = None,nextNode = None):
-        self.data =data
+        self.data = data
         self.nextNode = None
 
 
@@ -12,7 +12,16 @@ class LinkList :
 
     def __init__(self,head = None) :
         self.head = head 
-
+        #add node
+    def additem(self,item):
+        newNode = Node(item)
+        newNode.nextNode = self.head
+        self.head = newNode
+    def Traversal(self):
+        tmp = self.head
+        while(tmp != None):
+            print(tmp.data)
+            tmp =tmp.nextNode
 L = LinkList ()
 node1 = Node(75)
 node2 = Node(80)
@@ -25,3 +34,29 @@ node1.nextNode = node2
 node2.nextNode = node3
 node3.nextNode = node4
 node4.nextNode = node5
+'''
+temp = L.head
+
+print(temp.data)
+while (temp.nextNode != None):
+    temp = temp.nextNode
+    print(temp.data)
+    
+temp = L.head
+while (temp != None):
+    print(temp.data)
+    temp = temp.nextNode
+
+#print Node
+def printrecru(temp):
+    if (temp == None):
+        pass
+    else :
+        print(temp.data)
+        printrecru(temp.nextNode)
+        
+        '''
+L.additem(12)
+L.Traversal()
+
+
